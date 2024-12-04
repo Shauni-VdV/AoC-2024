777e1c5504cae525561b13c1b22a3e6c72e19450
Shauni Van de Velde <shauni.van.de.velde@innoss.be>
2024-12-04 12:11:27 +0100

    day 3 pt2

--- 03/solution.ts
+++ 03/solution.ts
@@ -17,3 +17,28 @@ filteredData.forEach((multiplication) => {
 })
 
 console.log(total);
+
+// Part 2
+
+let filteredData2 = [...data.matchAll(/(mul\(\d+,\d+\)|(do\(\))|don\'t\(\))/g)]
+
+console.log(filteredData2.map((x) => x[0]));
+
+let totalPt2 = 0;
+var add = true;
+filteredData2.map((match) => match[0]).forEach((command) => {
+    if(command == "do()") {
+        add = true;
+    } else if(command == "don't()") {
+        add = false;
+    } else {
+        if(add) {
+            var parts = command.split(',');
+            parts = parts.map(part => part.replace(/\D/g, ''));
+            totalPt2 += parseInt(parts[0]) * parseInt(parts[1]);
+        }
+    }
+
+});
+
+console.log(totalPt2);
