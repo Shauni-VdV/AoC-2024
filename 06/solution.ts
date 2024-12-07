fb0fe537c0d77f2d28e2d2889578a9866ef3739e
Shauni <shauni.vandevelde@gmail.com>
2024-12-07 11:18:25 +0100

    traversal met obstacles

--- 06/solution.ts
+++ 06/solution.ts
@@ -6,72 +6,66 @@ class Guard {
     currentRow: number;
     currentCol: number;
     currentDirection: Direction;
+    outOfBounds: boolean;
 
     move(grid: Tile[][]) : Tile[][]{
         
-        let currentTile = grid[this.currentCol][this.currentRow];
-        console.log('current tile', currentTile);
+        let currentTile: Tile = grid[this.currentRow][this.currentCol];
         let nextTile : Tile;
-        let nextPos: number[];
+
+        console.log('current tile', currentTile);
         switch(this.currentDirection){
             case Direction.UP:
-                nextTile = grid[this.currentCol][this.currentRow - 1];
-                nextPos = [ this.currentCol, this.currentRow - 1];
+                nextTile = grid[this.currentRow - 1][this.currentCol];
                 break;
             case Direction.DOWN:
-                nextTile = grid[this.currentCol][this.currentRow];
-                nextPos = [this.currentCol, this.currentRow];
-                break;
-            case Direction.LEFT:
-                nextTile = grid[this.currentCol - 1][this.currentRow];
-                nextPos = [this.currentCol - 1, this.currentRow];
-                break;
-            case Direction.RIGHT:
-                nextTile = grid[this.currentCol + 1][this.currentRow];
-                nextPos = [this.currentCol + 1, this.currentRow];
+                nextTile = grid[this.currentRow + 1][this.currentCol ];
 
                 break;
+            case Direction.LEFT:
+                nextTile = grid[this.currentRow][this.currentCol - 1];
+                break;
+            case Direction.RIGHT:
+                nextTile = grid[this.currentRow][this.currentCol + 1];
+                break;
         }
-        console.log('next position', nextPos);
+        console.log("Attempting to move to tile", nextTile)
+
         if(nextTile == undefined){
             console.log('next tile is undefined');
+            this.outOfBounds = true;
             return grid;
         }
         if(nextTile.Type == 1){
             // eerst draaien
-            console.log('next tile is obstacle, turning', nextTile);
+            console.log('next tile is obstacle, turning..');
             switch(this.currentDirection){
                 case Direction.UP:
+                    // turn right
                     this.currentDirection = Direction.RIGHT;
-                    // next tile aanpassen = 1 naar onder, 1 naar rechts
-                    nextPos = [this.currentCol + 1, this.currentRow + 1];
-                    nextTile = grid[nextPos[0]][nextPos[1]];
-
+                    nextTile = grid[this.currentRow][this.currentCol + 1];
                     break;
                 case Direction.DOWN:
+                    // turn left
                     this.currentDirection = Direction.LEFT;
-                    nextPos = [this.currentCol - 1, this.currentRow - 1];
-                    nextTile = grid[nextPos[0]][nextPos[1]];
-                  
+                    nextTile = grid[this.currentRow][this.currentCol - 1];
                     break;
                 case Direction.LEFT:
+                    // turn up
                     this.currentDirection = Direction.UP;
-                    nextPos = [this.currentCol + 1, this.currentRow - 1];
-                    nextTile = grid[nextPos[0]][nextPos[1]];
-               
+                    nextTile = grid[this.currentRow - 1][this.currentCol];
                     break;
                 case Direction.RIGHT:
+                    // turn down
                     this.currentDirection = Direction.DOWN;
-                    nextPos = [this.currentCol - 1, this.currentRow + 1];
-                    nextTile = grid[nextPos[0]][nextPos[1]];
-                   
+                    nextTile = grid[this.currentRow + 1][this.currentCol];
                     break;
             }
             // dan bewegen
             console.log('after turning, next tile is:', nextTile);
-            console.log('at pos:', nextPos);
-            this.currentRow = nextPos[1];
-            this.currentCol = nextPos[0];
+            this.currentRow = nextTile.rowIndex;
+            this.currentCol = nextTile.colIndex;
+
             currentTile.Type = 0; // 'huidige' tile is geen guard meer
             nextTile.isTraversedByGuard = true; // nieuwe tile is traversed
             nextTile.Type = 3; // nieuwe tile is guard
@@ -79,8 +73,8 @@ class Guard {
         } else {
             // enkel bewegen in huidige richting
             console.log('moving in current direction');
-            this.currentRow = nextPos[1];
-            this.currentCol = nextPos[0];
+            this.currentRow = nextTile.rowIndex;
+            this.currentCol = nextTile.colIndex;
             console.log('newX:', this.currentRow, 'newY:', this.currentCol);
             currentTile.Type = 0; // 'huidige' tile is geen guard meer
             nextTile.isTraversedByGuard = true; // nieuwe tile is traversed
@@ -100,6 +94,8 @@ enum Direction {
 }
 
 class Tile {
+    colIndex: number;
+    rowIndex: number;
     value: string;
     Type: number; // 0 = open, 1 = obstacle, 3 = guard
     isTraversedByGuard: boolean;
@@ -112,7 +108,7 @@ class Map {
     constructor(grid: Tile[][]){
         this.grid = grid;
         this.guard = findGuard(grid);
-        grid[this.guard.currentCol][this.guard.currentRow].isTraversedByGuard = true; // eerste tile is traversed
+        grid[this.guard.currentRow][this.guard.currentCol].isTraversedByGuard = true; // eerste tile is traversed
     }
 
     moveGuard(){
@@ -135,6 +131,9 @@ class Map {
             );
         });
     }
+    getTraversalCount(){
+        console.log("Tiles traversed: ", this.grid.flatMap((tile) => tile.filter((tile) => tile.isTraversedByGuard)).length);
+    }
 }
 
 function findGuard(grid: Tile[][]) : Guard{
@@ -145,8 +144,8 @@ function findGuard(grid: Tile[][]) : Guard{
             let tile: Tile = grid[row][col];
             if(tile.Type == 3){
                 let initGuard = new Guard();
-                initGuard.currentRow = col;
-                initGuard.currentCol = row;
+                initGuard.currentRow = row;
+                initGuard.currentCol = col;
                 initGuard.currentDirection = tile.value as Direction;
                 guard = initGuard;
             }
@@ -157,7 +156,7 @@ function findGuard(grid: Tile[][]) : Guard{
 
 
 (function solvePt1(){
-    let rowsWithTiles = rows.map((row) => row.map((tile, index) => {
+    let rowsWithTiles = rows.map((row, rowIndex) => row.map((tile, colIndex) => {
         let tileType : number;
         switch(tile){
             case '.':
@@ -170,17 +169,23 @@ function findGuard(grid: Tile[][]) : Guard{
                 tileType = 3;
                 break;
         }
-        return Object.assign(new Tile(), {value: tile, Type: tileType, isTraversedByGuard: false});
+        return Object.assign(new Tile(), {
+            value: tile, 
+            Type: tileType, 
+            isTraversedByGuard: false,
+            colIndex: colIndex,
+            rowIndex: rowIndex
+        });
     }));
         
 
     let map : Map = new Map(rowsWithTiles);
 
-    for(let i = 0; i < 10; i++){
+    while(!map.guard.outOfBounds){
         map.moveGuard();
         map.print();
-
     }
+    map.getTraversalCount();
 
     map.print();
 })();
