93a15b42d31bc607cf3ec5c344a672d0cf5d7aea
Shauni Van de Velde <shauni.van.de.velde@innoss.be>
2024-12-12 21:42:29 +0100

    day12 : find plots & area size

--- 12/solution.ts
+++ 12/solution.ts
@@ -22,45 +22,56 @@ class Plot {
 }
 
 (function solvePt1(){
-    let plots = rows.map((row, rowIndex) => row.map((col, colIndex) => new Coord(rowIndex, colIndex, col)));
-    printPlots(plots);
+    let grid = rows.map((row, rowIndex) => row.map((col, colIndex) => new Coord(rowIndex, colIndex, col)));
+    let plots: Plot[] = [];
     // Find area & perimeter of each area
     // = plants with adjoining coords in cardinal directions
     let plantsTraversed : Coord[] = [];
-    plots.forEach((row, rowIndex) => {
+    grid.forEach((row, rowIndex) => {
         row.forEach((col,colIndex) => {
             if(!plantsTraversed.find(plant => plant.row === rowIndex && plant.col === colIndex)){
                 let plantType = col.value;
-                let foundPlants = findArea(plots, rowIndex, colIndex, plantType, [col]);
+                let foundPlants = findArea(grid, rowIndex, colIndex, plantType, [new Coord(rowIndex, colIndex, plantType)]);
                 console.log("Found plants", foundPlants);
                 foundPlants.forEach(plant => plantsTraversed.push(plant));
+                plots.push(Object.assign(new Plot(), {
+                    plantCoords: foundPlants,
+                    plantType: plantType,
+                    area: foundPlants.length,
+                    perimeter: calculatePerimeter(foundPlants)
+                }));
             }
         });
     });
-})();
-// Calculate area and perimeter
+    printPlots(plots);
 
+})();
+// Calculate  perimeter
+function calculatePerimeter(plantCoords: Coord[]) : number{
+    
+    return 0;
+}
 
 // Helpers
 
-function findArea(plots: Coord[][], row: number, col: number, plantType: string, foundPlants: Coord[]) : Coord[]{
+function findArea(grid: Coord[][], row: number, col: number, plantType: string, foundPlants: Coord[]) : Coord[]{
     let adjoiningPlants : Coord[] = foundPlants || [];
     let directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
     directions.forEach(dir => {
         let newRow = row + dir[0];
         let newCol = col + dir[1];
-        if(0 <= newRow && newRow < plots.length && 0 <= newCol && newCol < plots[0].length){
-            let adjoiningPlant = plots[newRow][newCol];
+        if(0 <= newRow && newRow < grid.length && 0 <= newCol && newCol < grid[0].length && !adjoiningPlants.find(plant => plant.row === newRow && plant.col === newCol)){
+            let adjoiningPlant = grid[newRow][newCol];
             if(adjoiningPlant.value === plantType){
                 adjoiningPlants.push(adjoiningPlant);
-                findArea(plots, newRow, newCol, plantType, adjoiningPlants)
+                findArea(grid, newRow, newCol, plantType, adjoiningPlants)
             }
         }
     });
     return adjoiningPlants;
 }
-function printPlots(plots: Coord[][]){
-    plots.forEach(row => {
-        console.log(row.map(col => col.value).join(''));
+function printPlots(plots: Plot[]){
+    plots.forEach(plot => {
+        console.log("Plot of type", plot.plantType, "has area", plot.area, "and perimeter", plot.perimeter);
     });
 }
