3b1b45e1492e222baedd766bfccb79fc69ac4919
Shauni Van de Velde <shauni.van.de.velde@innoss.be>
2024-12-13 16:25:08 +0100

    day13: start pt1

--- 13/solution.ts
+++ 13/solution.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from 'fs';
 const data = readFileSync('../AOC_2024/13/input.txt', 'utf8');
+let machineLines = data.split('\r\n\r').map((machineLine) => machineLine.replace(/\r/g, '').split('\n').filter(line => line.length > 0));
 
 class Coord {
     X: number;
@@ -16,31 +17,51 @@ class Machine {
     PrizeCoord: Coord;
 }
 
-let machines: Machine[] = [];
-let machineLines = data.split('\r\n\r').map((machineLine) => machineLine.replace(/\r/g, '').split('\n').filter(line => line.length > 0));
-machineLines.forEach((machineLine) => {
-    let machine = new Machine();
-    // Find A Button values
-    let ABtn = new Button();
-    ABtn.XIncrease = parseInt(machineLine[0].split(' ')[2].replace(/\D/g, ''));
-    ABtn.YIncrease = parseInt(machineLine[0].split(' ')[3].replace(/\D/g, ''));
+let machines: Machine[] = mapInput();
 
-    // Find B Button values
-    let BBtn = new Button();
-    BBtn.XIncrease = parseInt(machineLine[1].split(' ')[2].replace(/\D/g, ''));
-    BBtn.YIncrease = parseInt(machineLine[1].split(' ')[3].replace(/\D/g, ''));
+// Part 1
+(function solvePt1(){
+    let totalWins = 0;
+    machines.forEach((machine) => {
+        findOptimalPresses(machine);
 
-    // Find Prize
-    let prize = new Coord();
-    prize.X = parseInt(machineLine[2].split(' ')[1].replace(/\D/g, ''));
-    prize.Y = parseInt(machineLine[2].split(' ')[2].replace(/\D/g, ''));
+    })
+})
 
-    machine.A = ABtn;
-    machine.B = BBtn;
-    machine.PrizeCoord = prize;
-    
-    machines.push(machine);
-    
-});
-console.log("Machine lines", machineLines);
-console.log('Machines', machines);
+
+function findOptimalPresses(machine: Machine){
+    let APresses = 0;
+    let BPresses = 0;
+}
+
+// Helpers
+function mapInput() : Machine[]{
+    machineLines.forEach((machineLine) => {
+        let machine = new Machine();
+        // Find A Button values
+        let ABtn = new Button();
+        ABtn.XIncrease = parseInt(machineLine[0].split(' ')[2].replace(/\D/g, ''));
+        ABtn.YIncrease = parseInt(machineLine[0].split(' ')[3].replace(/\D/g, ''));
+
+        // Find B Button values
+        let BBtn = new Button();
+        BBtn.XIncrease = parseInt(machineLine[1].split(' ')[2].replace(/\D/g, ''));
+        BBtn.YIncrease = parseInt(machineLine[1].split(' ')[3].replace(/\D/g, ''));
+
+        // Find Prize
+        let prize = new Coord();
+        prize.X = parseInt(machineLine[2].split(' ')[1].replace(/\D/g, ''));
+        prize.Y = parseInt(machineLine[2].split(' ')[2].replace(/\D/g, ''));
+
+        machine.A = ABtn;
+        machine.B = BBtn;
+        machine.PrizeCoord = prize;
+        
+        machines.push(machine);
+        
+    });
+    console.log("Machine lines", machineLines);
+    console.log('Machines', machines);
+    return machines;
+}
+
